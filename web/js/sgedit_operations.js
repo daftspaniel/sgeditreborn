bce2c8b989b01870e022e806d8ec489651733f0b
Davy Mitchell <daftspaniel@gmail.com>
2018-10-30 13:49:24 +0000

    Clear dialog working. Clear with byte option.

--- web/js/sgedit_operations.js
+++ web/js/sgedit_operations.js
@@ -1,10 +1,18 @@
 function clearScreen() {
+    console.log('CLEAR SCREEN')
     if (getById('cls-option-basic').checked) {
         let requestedValue = getById('cls').value
         state.screen.reset_data(requestedValue)
-        return
+        //return
+        updateEditorWithData()
+    }
+    else if (getById('cls-option-byte').checked) {
+        let requestedValue = getById('cls-byte').value
+        state.screen.reset_data(intToHex(requestedValue))
+        //return
+        updateEditorWithData()
     }
 
-    state.screen.reset_data('89')
+    //state.screen.reset_data('89')
     hideById('clear-dialog')
 }
